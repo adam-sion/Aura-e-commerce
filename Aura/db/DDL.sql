f68b933d3383968d08bb02866b37263acd3cd059
adam-sion <adamsion74@gmail.com>
2024-10-09 22:27:13 +0300

    start db

---
+++ Aura/db/DDL.sql
@@ -0,0 +1,43 @@
+CREATE SCHEMA IF NOT EXISTS aura
+
+DROP TABLE IF EXISTS aura.products CASCADE
+
+CREATE TABLE aura.products (
+ id SERIAL PRIMARY KEY,
+ name TEXT NOT NULL,
+ price SMALLINT NOT NULL,
+ gender VARCHAR(10) CHECK (gender IN ('men', 'women', 'kids')),
+ img TEXT NOT NULL
+)
+
+
+DROP TABLE IF EXISTS aura.customers CASCADE
+
+CREATE TABLE aura.customers (
+ id SERIAL PRIMARY KEY,
+ username VARCHAR(20) NOT NULL,
+ password VARCHAR(255) NOT NULL
+)
+
+
+DROP TABLE IF EXISTS aura.orders CASCADE
+
+CREATE TABLE aura.orders (
+ id SERIAL PRIMARY KEY,
+ customer_id INTEGER NOT NULL REFERENCES aura.customers(id)
+    ON DELETE CASCADE,
+ amount SMALLINT NOT NULL
+)
+
+
+DROP TABLE IF EXISTS aura.order_items CASCADE
+
+CREATE TABLE aura.order_items (
+ id SERIAL PRIMARY KEY,
+ order_id NOT NULL INTEGER REFERENCES aura.orders(id)
+    ON DELETE CASCADE,
+ product_id NOT NULL INTEGER REFERENCES aura.products(id)
+    ON DELETE CASCADE,
+ quantity SMALLINT NOT NULL
+)
+
