f7196aaa227842e51ce42fec8beb7e168d99fd77
Adam <adamsion74@gmail.com>
2024-10-18 18:26:58 +0300

    add buttton

--- Aura/front/src/views/AdminDash/AdminDash.tsx
+++ Aura/front/src/views/AdminDash/AdminDash.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { isAdmin } from "../../utils/roles";
 import { Navigate } from "react-router-dom";
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import { categories } from "../../utils/data";
 import "./AdminDash.css";
 import beachBack from "../../Assets/categoriesBackground.png";
@@ -57,25 +57,28 @@ export const AdminDash:FC = ()=> {
         <Grid item sm={4} xs={4} md={4} key={category.name} sx={{ height: '100%' }}>
           <Box sx={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', overflow: 'hidden' }} className="category-card">
     <img style={{ borderRadius: '20px' }} height={'90%'} width={'90%'} src={category.pic} alt="" />
-    <Box 
-        sx={{ 
-            position: 'absolute', 
-            top: '50%', 
-            left: '50%', 
-            transform: 'translate(-50%, -50%)', // Center the text
-            color: 'white', 
-            fontSize: '24px', // Adjust font size as needed
-            opacity: 0, // Start hidden
-            transition: 'opacity 0.3s ease', // Smooth transition
-            backgroundColor: 'rgba(0, 0, 0, 0.7)', // Optional: background for better readability
-            padding: '10px 20px', // Optional: padding around text
-            borderRadius: '10px', // Optional: rounded corners
-            textAlign: 'center' // Center text
-        }} 
-        className="category-name"
-    >
-        {category.name}
-    </Box>
+
+    <Button 
+    onClick={()=> alert('hiii')}
+    sx={{ 
+        position: 'absolute', 
+        top: '50%', 
+        left: '50%', 
+        transform: 'translate(-50%, -50%)',
+        color: 'white', 
+        fontSize: '24px', 
+        opacity: 0, 
+        transition: 'opacity 0.3s ease', 
+        backgroundColor: 'rgba(0, 0, 0, 0.7)', 
+        padding: '10px 20px', 
+        borderRadius: '10px', 
+        textAlign: 'center' 
+    }} 
+    className="category-name"
+>
+    {category.name}
+</Button>
+
 </Box>
 
         </Grid>
