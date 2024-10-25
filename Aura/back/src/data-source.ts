f20a3bd8cc20b736871d3e5e84c5bc6a969f5174
Adam <adamsion74@gmail.com>
2024-10-25 16:14:21 +0300

    serve static files4

--- Aura/back/src/data-source.ts
+++ Aura/back/src/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME || "postgres",
 
 
-  ssl: { rejectUnauthorized: false } ,
+  ssl: false ,
 
   entities: [path.join(__dirname, 'entities', '**/*.entity.{ts,js}')],
   
