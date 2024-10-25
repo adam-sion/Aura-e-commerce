cebf5f2d62e4f11f705a5bdedc9c4b1b1b959a7e
Adam <adamsion74@gmail.com>
2024-10-25 16:09:13 +0300

    serve static files4

--- Aura/back/src/data-source.ts
+++ Aura/back/src/data-source.ts
@@ -3,21 +3,21 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import * as path from 'path';
 import 'reflect-metadata';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost", 
-  port: +process.env.DB_PORT! || 5432, 
-  username: process.env.DB_USERNAME || "postgres", 
-  password: process.env.DB_PASSWORD || "postgres", 
-  database: process.env.DB_NAME || "postgres", 
+  host: process.env.DB_HOST || "localhost",
+  port: +process.env.DB_PORT! || 5432,
+  username: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "postgres",
 
-  // Ensure you adjust the SSL settings for Supabase
-  ssl: {
-    rejectUnauthorized: false,
-  },
+
+  ssl: isProd? { rejectUnauthorized: true }:false ,
 
   entities: [path.join(__dirname, 'entities', '**/*.entity.{ts,js}')],
   
-  schema: "aura", 
+  schema: "aura",
   namingStrategy: new SnakeNamingStrategy(),
 });
