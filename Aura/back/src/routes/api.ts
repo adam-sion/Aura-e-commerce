d2f458e6a6b157d98e39ebb8dc229945eafa5967
Adam <adamsion74@gmail.com>
2024-10-26 14:02:00 +0300

    finish vercel without imgs

--- Aura/back/src/routes/api.ts
+++ Aura/back/src/routes/api.ts
@@ -20,13 +20,16 @@ APIRouter.get('/product/:id', getProductByIdHandler);
 APIRouter.post('/product', addProductHandler);
 APIRouter.delete('/product/:id', deleteProductHandler);
 
-// Use memory storage for multer
-const storage = multer.memoryStorage();  
-const upload = multer({ storage });  
+const storage = multer.diskStorage({
+    destination:'./tmp/images',
+    filename: (req, file, cb) => {
+        const uniqueName = `${file.fieldname}_${Date.now()}_${Math.round(Math.random() * 1e9)}${path.extname(file.originalname)}`;
+        cb(null, uniqueName);
+    }
+});
 
-// Define upload route
-APIRouter.post('/upload', upload.single('product'), uploadImageHandler);
 
+const upload = multer({ storage });
 
 
 APIRouter.post('/upload', upload.single('product'), uploadImageHandler);
